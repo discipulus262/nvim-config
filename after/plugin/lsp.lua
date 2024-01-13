2f72ae65e96c61ef3d7dc2ee6d88c393857ddd74
discipulus262 <133603539+discipulus262@users.noreply.github.com>
2024-01-13 21:40:51 +0000

    Add files via upload

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,16 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+---
+-- Replace these language servers
+-- with the ones you have installed in your system
+---
+require('lspconfig').tsserver.setup({})
+require('lspconfig').clangd.setup({})
+require('lspconfig').eslint.setup({})
+require('lspconfig').fortls.setup({})
