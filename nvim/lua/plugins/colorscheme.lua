6561f72f3d450c73a260b186c4d53ee1f66ddc84
discipulus262 <133603539+discipulus262@users.noreply.github.com>
2024-11-01 18:08:28 -0400

    Add files via upload

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
